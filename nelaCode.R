9aef2c20661aa32b1febbd175b859c80917c6cf7
Nela Riddle <nelariddle@gmail.com>
2022-11-02 13:30:09 -0400

    made a table thing

--- nelaCode.R
+++ nelaCode.R
@@ -8,7 +8,16 @@ data$PCT.GPA<-data$GPA.GRADES/data$TOTAL.GRADES
 data<-data%>%filter(LETTER!="Y"&LETTER!="G")
 # only undergrad
 data<-data%>%filter(COURSE.<500)
-# dont care about 100 level
-data<-data%>%filter(COURSE.>199)
 
+# linear model
 summary(lm(data$AVG.SECT.GPA~data$NEWSALARY+data$YEAR+data$COURSE.+data$PCT.GPA+data$PERCENT.MAJORS))
+
+# total grades, gpa grades given by course
+aggregate(data$GPA.GRADES, by=list(course=data$COURSE.), FUN=sum)
+grade.by.course<-aggregate(cbind(data$GPA.GRADES,data$TOTAL.GRADES), by=list(course=data$COURSE.), FUN=sum)
+grade.by.course<-cbind(grade.by.course,grade.by.course[,2]/grade.by.course[,3])
+colnames(grade.by.course)[4]<-"percent.gpa"
+colnames(grade.by.course)[3]<-"total"
+colnames(grade.by.course)[2]<-"gpa"
+
+
