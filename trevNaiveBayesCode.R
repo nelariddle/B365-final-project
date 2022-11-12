6bb27a00e7c15bcc18ab1ff847e5f4bd9d689e94
Buechler, Trevor E <tbuechle@iu.edu>
2022-11-12 17:24:31 -0500

    Update trevNaiveBayesCode.R
    No longer scaled and using useful classes

--- trevNaiveBayesCode.R
+++ trevNaiveBayesCode.R
@@ -12,13 +12,29 @@ library(caret)
 data = read.csv("data-with-salary-class.csv")
 dimnames(data)
 
+library(dplyr)
+
+# Data cleaning from Nela
+# add a column for percent GPA grades
+data$PCT.GPA <- data$GPA.GRADES / data$TOTAL.GRADES
+# dont care about career and thesis courses
+data <- data %>% filter(LETTER != "Y" & LETTER != "G")
+# only undergrad
+data <- data %>% filter(COURSE. < 500)
+
+## More Data cleaning
+# Select only important columns
+data = data[,c("YEAR", "COURSE.", "PERCENT.MAJORS", "NEWSALARY", "AVG.SECT.GPA", "SALARYCLASS")]
+# Flooring the course numbers to only 1,2,3, and 4
+data["COURSE."] = floor(data["COURSE."] / 100)
+
 split <- sample.split(data, SplitRatio = 0.5)
 train <- subset(data, split == "TRUE")
 test <- subset(data, split == "FALSE")
 train
 test
-train_scale <- scale(train[, 17:38])
-test_scale <- scale(test[, 17:38])
+#train_scale <- scale(train[, 5])
+#test_scale <- scale(test[, 5])
 
 set.seed(120)
 classifier_cl <- naiveBayes(SALARYCLASS ~ ., data = train)
