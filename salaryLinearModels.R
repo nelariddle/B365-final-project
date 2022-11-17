216e81c96c56efb7c3f6fc14808c24e5f9fb4bce
Nela Riddle <nelariddle@gmail.com>
2022-11-17 00:15:41 -0500

    more linear modelz

---
+++ salaryLinearModels.R
@@ -0,0 +1,111 @@
+setwd("C:/Users/nelar/OneDrive/Documents/fall 2022/b365/Final project")
+data <-
+  read.csv("data-with-salary-class.csv", stringsAsFactors = TRUE)
+# install.packages("dplyr")
+# install.packages("english")
+library(dplyr)
+library(english)
+
+# add a column for percent GPA grades
+data$PCT.GPA <- data$GPA.GRADES / data$TOTAL.GRADES
+# dont care about career and thesis courses
+data <- data %>% filter(LETTER != "Y" & LETTER != "G")
+# only undergrad
+data <- data %>% filter(COURSE. < 500)
+
+# create instructor data
+# TOTAL.GRADES.INSTRUCTOR is the total # of grades given by an instructor that year
+# AVG.GPA INSTRUCTOR is the average GPA of grades given by an instructor that year
+instructor.data <-
+  data %>% group_by(INSTRUCTOR.NAME, YEAR) %>% mutate(
+    TOTAL.INSTRUCTOR.GRADES =
+      sum(TOTAL.GRADES),
+    AVG.INSTRUCTOR.GPA =
+      sum(AVG.SECT.GPA *
+            TOTAL.GRADES) /
+      sum(TOTAL.GRADES)
+  ) %>% ungroup()
+
+# single variable analysis
+library(english)
+X <-
+  instructor.data %>% select(
+    TOTAL.INSTRUCTOR.GRADES,
+    AVG.SECT.GPA,
+    YEAR,
+    COURSE.,
+    PERCENT.MAJORS,
+    AVG.INSTRUCTOR.GPA,
+    AVG.STDNT.CUM.GPA
+  ) %>% as.matrix()
+y <- instructor.data %>% select(NEWSALARY) %>% as.matrix()
+n_attrs <- ncol(X)
+sses <- rep(0, n_attrs)
+attr_perf<-array(0,dim=c(n_attrs,4))
+for (i in 1:n_attrs) {
+  plot(X[, i], y, xlab = colnames(X)[i], ylab = colnames(y)[1])
+  d <- cbind(X[, i] ^ 0, X[, i] ^ 1)
+  a <- solve(t(d) %*% d, t(d) %*% y)
+  abline(a[1], a[2], col = "purple")
+  yhat <- d %*% a
+  error <- y - yhat
+  sse <- sum(error ^ 2)
+  sses[i] <- sse
+  attr_perf[i,]<-c(colnames(X)[i],round(sse, 3),a[1],a[2])
+}
+for (rank in order(sses)) {
+  cat(
+    attr_perf[rank, 1],
+    "\nerror:",
+    round(as.numeric(attr_perf[rank, 2], 3)),
+    "intercept:",
+    round(as.numeric (attr_perf[rank, 3]), 3),
+    "slope:",
+    round  (as.numeric (attr_perf[rank, 4]), 3),
+    "\n"
+  )
+}
+
+# variable selection
+X <- instructor.data %>% select(
+  TOTAL.INSTRUCTOR.GRADES,
+  AVG.SECT.GPA,
+  YEAR,
+  COURSE.,
+  PERCENT.MAJORS,
+  AVG.INSTRUCTOR.GPA,
+  AVG.STDNT.CUM.GPA
+) %>% as.matrix()
+y <- instructor.data %>% select(NEWSALARY) %>% as.matrix()
+n_attrs <- ncol(X)
+used <-
+  rep(FALSE, n_attrs)
+var <- rep(0, n_attrs)
+bestsse <-
+  rep(10000000000000, n_attrs)
+for (j in 1:n_attrs)  {
+  for (i in which(used == FALSE)) {
+    used[i] <- TRUE
+    XX <- cbind(X[, 1] ^ 0, X[, used])
+    a <- solve(t(XX) %*% XX , t(XX) %*% y)
+    yhat <- XX %*% a
+    error <- y - yhat
+    sse <- sum(error ^ 2)
+    if (sse < bestsse[j]) {
+      bestsse[j] <- sse
+      var[j] <- i
+    }
+    used[i] <- FALSE
+    
+  }
+  used[var[j]] <-
+    TRUE
+  cat("the", ordinal(j), "best attribute is", colnames(X)[j], '\n')
+}
+plot(bestsse,
+     main = "squared error vs. num attributes used",
+     xlab = "num attributes",
+     ylab = "squared error")
+
+
+
