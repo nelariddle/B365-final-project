a158ff36862a87a4cac008d058d6752b68cd041f
Nela Riddle <nelariddle@gmail.com>
2022-11-16 22:23:31 -0500

    wrote code myself, definitely did not steal prof's code

--- nelaCode.R
+++ nelaCode.R
@@ -10,42 +10,14 @@ data$PCT.GPA <- data$GPA.GRADES / data$TOTAL.GRADES
 data <- data %>% filter(LETTER != "Y" & LETTER != "G")
 # only undergrad
 data <- data %>% filter(COURSE. < 500)
-#data <- data %>% filter(COURSE. > 199)
-# salary class
-#data$bruh<-floor((data$NEWSALARY-75000)/25000)+1
 
-# linear model
+# practice linear model
 summary(
   lm(
     data$NEWSALARY ~ data$AVG.SECT.GPA + data$YEAR + data$COURSE. + data$PCT.GPA +
-      data$PERCENT.MAJORS + data$LETTER
+      data$PERCENT.MAJORS
   )
 )
-
-# linear model 2
-useful.data <-
-  instructor.data %>% select(
-    YEAR,
-    INSTRUCTOR.NAME,
-    LETTER,
-    COURSE.,
-    TOTAL.GRADES,
-    AVG.SECT.GPA,
-    AVG.STDNT.CUM.GPA,
-    PERCENT.MAJORS,
-    TOTAL.GRADES.INSTRUCTOR,
-    AVG.GPA.INSTRUCTOR,
-    NEWSALARY
-  )
-for (i in 1:7) {
-  X <- as.matrix(useful.data[, 4:10])[,-i]
-  y <- as.matrix(useful.data[, 11])
-  a <- solve(t(X) %*% X, t(X) %*% y)
-  yhat <- X %*% a
-  error <- y - yhat
-  sse <- sum(error ^ 2)
-  cat("sse from omitting", colnames(useful.data)[i + 3], ":", sse, "\n")
-}
 
 # create instructor data
 instructor.data <-
@@ -56,7 +28,7 @@ instructor.data <-
       sum(AVG.SECT.GPA *
             TOTAL.GRADES) /
       sum(TOTAL.GRADES)
-  )
+  ) %>% ungroup()
 
 # group by instructor, plot total grades
 instructor.data %>%
@@ -67,14 +39,102 @@ instructor.data %>%
 instructor.data %>% ggplot(aes(AVG.GPA.INSTRUCTOR, NEWSALARY, color = YEAR)) + geom_point()
 
 # linear model with total grades
-fit <- lm(data = instructor.data
-          , NEWSALARY ~ TOTAL.GRADES.INSTRUCTOR)
+fit <- lm(
+  data = instructor.data
+  ,
+  NEWSALARY ~ TOTAL.GRADES.INSTRUCTOR + AVG.SECT.GPA + YEAR + COURSE. + PCT.GPA +
+    PERCENT.MAJORS
+)
+summary(fit)
+
+# single variable analysis
+library(english)
+X <-
+  instructor.data %>% select(
+    TOTAL.GRADES.INSTRUCTOR,
+    AVG.SECT.GPA,
+    YEAR,
+    COURSE.,
+    PCT.GPA,
+    PERCENT.MAJORS,
+    AVG.GPA.INSTRUCTOR,
+    AVG.STDNT.CUM.GPA
+  ) %>% as.matrix()
+y <- instructor.data %>% select(NEWSALARY) %>% as.matrix()
+n_attrs <- ncol(X)
+sses <- rep(0, n_attrs)
+for (i in 1:n_attrs) {
+  plot(X[, i], y, xlab = colnames(X)[i], ylab = colnames(y)[1])
+  d <- cbind(X[, i] ^ 0, X[, i] ^ 1)
+  a <- solve(t(d) %*% d, t(d) %*% y)
+  abline(a[1], a[2], col = "purple")
+  yhat <- d %*% a
+  error <- y - yhat
+  sse <- sum(error ^ 2)
+  sses[i] <- sse
+  cat(
+    "sse for",
+    colnames(X)[i],
+    "is",
+    round(sse, 3),
+    "with intercept",
+    a[1],
+    "and slope",
+    a[2],
+    "\n"
+  )
+}
+i <- 1
+for (attr in colnames(X)[order(sses)]) {
+  cat("the", ordinal(i), "best attribute is", attr, "\n")
+  i = i + 1
+}
+
+# variable selection
+X <- instructor.data %>% select(
+  TOTAL.GRADES.INSTRUCTOR,
+  AVG.SECT.GPA,
+  YEAR,
+  COURSE.,
+  PCT.GPA,
+  PERCENT.MAJORS,
+  AVG.GPA.INSTRUCTOR,
+  AVG.STDNT.CUM.GPA
+) %>% as.matrix()
+y <- instructor.data %>% select(NEWSALARY) %>% as.matrix()
+n_attrs <- ncol(X)
+used <-
+  rep(FALSE, n_attrs)
+var <- rep(0, n_attrs)
+bestsse <-
+  rep(10000000000000, n_attrs)
+for (j in 1:n_attrs)  {
+  for (i in which(used == FALSE)) {
+    used[i] <- TRUE
+    XX <- cbind(X[, 1] ^ 0, X[, used])
+    a <- solve(t(XX) %*% XX , t(XX) %*% y)
+    yhat <- XX %*% a
+    error <- y - yhat
+    sse <- sum(error ^ 2)
+    if (sse < bestsse[j]) {
+      bestsse[j] <- sse
+      var[j] <- i
+    }
+    used[i] <- FALSE
+    
+  }
+  used[var[j]] <-
+    TRUE
+  cat("the", ordinal(j), "best attribute is", colnames(X)[j], '\n')
+}
+plot(bestsse,
+     main = "squared error vs. num attributes used",
+     xlab = "num attributes",
+     ylab = "squared error")
+
 
 
 # total grades, gpa grades given by course
-aggregate(data$GPA.GRADES,
-          by = list(course = data$COURSE.),
-          FUN = sum)
 grade.by.course <-
   aggregate(
     cbind(data$GPA.GRADES, data$TOTAL.GRADES),
@@ -90,7 +150,7 @@ colnames(grade.by.course)[2] <- "gpa"
 # tree
 library(rpart)
 tree <-
-  rpart(data$NEWSALARY ~ data$AVG.SECT.GPA + data$COURSE. + data$PERCENT.MAJORS)
+  rpart(data=instructor.data,NEWSALARY ~ TOTAL.GRADES.INSTRUCTOR,AVG.SECT.GPA + COURSE. + PERCENT.MAJORS)
 plot(tree, uniform = TRUE, margin = 0.2)
 text(tree,
      use.n = TRUE,
